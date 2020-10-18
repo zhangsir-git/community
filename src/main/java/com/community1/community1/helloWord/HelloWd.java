f0577bef86c2781838f4f2c94725ea4f4360f53f
zhangsir-git <1256317001@qq.com>
2020-10-19 00:03:48 +0800

    main

--- src/main/java/com/community1/community1/helloWord/HelloWd.java
+++ src/main/java/com/community1/community1/helloWord/HelloWd.java
@@ -7,13 +7,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWd {
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name="name",value = "value") String name, Model model){
+    @GetMapping("/test")
+    public String hello(@RequestParam(name="name") String name, Model model){
           model.addAttribute("name",name);
 
 
 
-          return "hello";
+          return "test";
     }
 
 }
--- src/test/java/com/community1/community1/Community1ApplicationTests.java
+++ src/test/java/com/community1/community1/Community1ApplicationTests.java
@@ -1,13 +1,2 @@
-package com.community1.community1;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class Community1ApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
